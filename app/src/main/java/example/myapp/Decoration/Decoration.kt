580dd09027c5cdf259c2f0412735d08f1f9235fd
HrsCode11 <hariscahya35@gmail.com>
2024-03-19 00:38:55 +0700

    tugas Lesson3 4-selesai

---
+++ app/src/main/java/example/myapp/Decoration/Decoration.kt
@@ -0,0 +1,41 @@
+package example.myapp.Decoration
+
+data class Decoration(val rocks: String) {
+}
+fun makeDecoration(){
+    val decoration1= Decoration("granite")
+    println(decoration1)
+
+    val decoration2 = Decoration("slate")
+    println(decoration2)
+
+    val decoration3 = Decoration("slate")
+    println(decoration3)
+
+    println (decoration1.equals(decoration2))
+    println (decoration3.equals(decoration2))
+}
+
+data class Decoration2(val rocks: String, val wood: String, val diver: String){
+}
+
+fun makeDecorations() {
+    val d5 = Decoration2("crystal", "wood", "diver")
+    println(d5)
+
+    val (rock, _, diver) = d5
+    println(rock)
+    println()
+    println(diver)
+}
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+fun main(){
+    makeDecoration()
+    makeDecorations()
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
+}
--- app/src/main/java/example/myapp/Main.kt
+++ app/src/main/java/example/myapp/Main.kt
@@ -1,25 +1,23 @@
 package example.myapp
 
 fun buildAquarium() {
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
-    myAquarium.height = 60
-    myAquarium.printSize()
+    val aquarium8 = Aquarium(length = 25, width = 25, height = 40)
+    aquarium8.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
+}
 
-    val aquarium1 = Aquarium()
-    aquarium1.printSize()
-    val aquarium2 = Aquarium(width = 25)
-    aquarium2.printSize()
-    val aquarium3 = Aquarium(height = 35, length = 110)
-    aquarium3.printSize()
-    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
-    aquarium4.printSize()
+fun makeFish(){
+    val shark=Shark()
+    val pleco=Plecostomus()
 
-    val aquarium6 = Aquarium(numberOfFish = 29)
-    aquarium6.printSize()
-    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
 }
 
 fun main(){
     buildAquarium()
+    makeFish()
 }
